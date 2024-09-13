f67dbbdc85a189d8fd53d7166ab733252afb132e
Raushan <lcs2021032@iiitl.ac.in>
2024-09-13 22:12:03 +0530

    Modified API Key

--- src/Config/FirebaseConfig.js
+++ src/Config/FirebaseConfig.js
@@ -1,10 +1,10 @@
 const FirebaseConfig = {
-    apiKey: "AIzaSyAEN2QDVcpFR2AaAIjwPzb3tCBuxST6nBU",
-    authDomain: "crypto-simulator-ea32d.firebaseapp.com",
-    projectId: "crypto-simulator-ea32d",
-    storageBucket: "crypto-simulator-ea32d.appspot.com",
-    messagingSenderId: "483362661008",
-    appId: "1:483362661008:web:937053838cdf2de67f2e15"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
 
-  export default FirebaseConfig 
+export default FirebaseConfig;
